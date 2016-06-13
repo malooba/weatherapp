ec6dcfd18d9aaa4ad09b89030c6acc6ea9bce5b3
andybisson <microsoft@abisson.co.uk>
2016-06-13 10:52:16 +0100

    Initial Commit

---
+++ Weather/Models/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Weather.Models
+{
+    public class WeatherData
+    {
+        private readonly string ep = WebConfigurationManager.AppSettings["azureEndpoint"];
+        public IEnumerable<WeatherEntity> GetWeatherData()
+        {
+            var storageAccount = CloudStorageAccount.Parse(ep);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference("esp8266");
+            var query = (new TableQuery<WeatherEntity>()).Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "ESP8266"));
+            var w = table.ExecuteQuery<WeatherEntity>(query);
+            return w;
+        }
+    } 
+}
---
+++ Weather/Models/WeatherEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Weather.Models
+{
+    public class WeatherEntity : TableEntity
+    {
+        public WeatherEntity(string partition, DateTime utc)
+        {
+            PartitionKey = partition;
+            RowKey = utc.ToString("yyyy-MM-ddTHH:mm:ss.fffffff");
+        }
+
+        public override void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            Utc = properties["Utc"].DateTime.Value;
+            Celsius = properties["Celsius"].DoubleValue.Value;
+            Humidity = properties["Humidity"].DoubleValue.Value;
+            Pressure = properties["hPa"].Int64Value.Value;
+        }
+
+        public WeatherEntity() { }
+
+        public DateTime Utc { get; set; }
+
+        public double Celsius { get; set; }
+
+        public double Humidity { get; set; }
+
+        public long Pressure { get; set; }
+    }
+}
